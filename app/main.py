e021f3d6dcca887bafa9be5a46bcc4990cf7beba
kirill <krasikokirill@yandex.ru>
2026-01-20 02:31:23 +0300

    Add CORS middleware to FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from routers import main_router
@@ -19,6 +20,14 @@ app = FastAPI(
     redoc_url="/redoc",
     openapi_url="/openapi.json",
     root_path="/chess_service"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
